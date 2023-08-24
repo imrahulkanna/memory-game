71de30e897155b0c12c033cdfed3bea06386cd73
Rahul Kanna <67679039+RahulKanna05@users.noreply.github.com>
2023-08-24 08:31:28 +0530

    moved dfs & characters to public folder

--- src/data.js
+++ src/data.js
@@ -2,52 +2,52 @@ export const characters = [
   {
     id: 0,
     name: 'Monkey D. Luffy',
-    picSrc: 'src/assets/characters/0.webp'
+    picSrc: '/characters/0.webp'
   },
   {
     id: 1,
     name: 'Eustass Kid',
-    picSrc: 'src/assets/characters/1.webp'
+    picSrc: '/characters/1.webp'
   },
   {
     id: 2,
     name: 'Marshall D. Teach',
-    picSrc: 'src/assets/characters/2.webp'
+    picSrc: '/characters/2.webp'
   },
   {
     id: 3,
     name: 'Trafalgar D. Water Law',
-    picSrc: 'src/assets/characters/3.webp'
+    picSrc: '/characters/3.webp'
   },
   {
     id: 4,
     name: 'Boa Hancock',
-    picSrc: 'src/assets/characters/4.webp'
+    picSrc: '/characters/4.webp'
   },
   {
     id: 5,
     name: 'Sabo',
-    picSrc: 'src/assets/characters/5.webp'
+    picSrc: '/characters/5.webp'
   },
   {
     id: 6,
     name: 'Tony Tony Chopper',
-    picSrc: 'src/assets/characters/6.webp'
+    picSrc: '/characters/6.webp'
   },
   {
     id: 7,
     name: 'Kaidou',
-    picSrc: 'src/assets/characters/7.webp'
+    picSrc: '/characters/7.webp'
   },
   {
     id: 8,
     name: 'Nico Robin',
-    picSrc: 'src/assets/characters/8.webp'
+    picSrc: '/characters/8.webp'
   },
   {
     id: 9,
     name: 'Brook',
-    picSrc: 'src/assets/characters/9.webp'
+    picSrc: '/characters/9.webp'
   },
 ]
 
@@ -55,51 +55,51 @@ export const dfs = [
   {
     id: 0,
     name: 'Gomu Gomu no Mi',
-    picSrc: 'src/assets/dfs/0.webp'
+    picSrc: '/dfs/0.webp'
   },
   {
     id: 1,
     name: 'Jiki Jiki no Mi',
-    picSrc: 'src/assets/dfs/1.webp'
+    picSrc: '/dfs/1.webp'
   },
   {
     id: 2,
     name: 'Yami Yami no Mi',
-    picSrc: 'src/assets/dfs/2.webp'
+    picSrc: '/dfs/2.webp'
   },
   {
     id: 3,
     name: 'Ope Ope no Mi',
-    picSrc: 'src/assets/dfs/3.webp'
+    picSrc: '/dfs/3.webp'
   },
   {
     id: 4,
     name: 'Mero Mero no Mi',
-    picSrc: 'src/assets/dfs/4.webp'
+    picSrc: '/dfs/4.webp'
   },
   {
     id: 5,
     name: 'Mera Mera no Mi',
-    picSrc: 'src/assets/dfs/5.webp'
+    picSrc: '/dfs/5.webp'
   },
   {
     id: 6,
     name: 'Hito Hito no Mi',
-    picSrc: 'src/assets/dfs/6.webp'
+    picSrc: '/dfs/6.webp'
   },
   {
     id: 7,
     name: 'Uo Uo no Mi, Model: Seiryu',
-    picSrc: 'src/assets/dfs/7.webp'
+    picSrc: '/dfs/7.webp'
   },
   {
     id: 8,
     name: 'Hana Hana no Mi',
-    picSrc: 'src/assets/dfs/8.webp'
+    picSrc: '/dfs/8.webp'
   },
   {
     id: 9,
     name: 'Yomi Yomi no Mi',
-    picSrc: 'src/assets/dfs/9.webp'
+    picSrc: '/dfs/9.webp'
   },
 ]
